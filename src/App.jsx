6d257c10a5a0ed3416c57c1580edba24983d182b
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-11-12 15:12:05 -0500

    Start portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+// Hooks
+// import useTheme from "./hooks/useTheme";
+// import useAccent from "./hooks/useAccent";
+// import useLang from "./hooks/useLang";
+
 function App() {
+  // const { dark, toggle } = useTheme();
+  // const { accent, setAccent } = useAccent();
+  // const { lang, t, toggleLang } = useLang();
+
   return (
     <>
-      <div>
+      <div className="bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-white dark:from-neutral-950 via-white dark:via-neutral-950 to-white dark:to-neutral-950 min-h-screen text-neutral-900 dark:text-neutral-100">
         <h1>Portfolio's Matt</h1>
       </div>
     </>
