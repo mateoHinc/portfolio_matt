71100df7f9f0f4f2799f1e44fdf16de2f00d0e1b
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-11-13 12:24:03 -0500

    Add component Header in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 // Hooks
-// import useTheme from "./hooks/useTheme";
-// import useAccent from "./hooks/useAccent";
-// import useLang from "./hooks/useLang";
+import useTheme from "./hooks/useTheme";
+import useAccent from "./hooks/useAccent";
+import useLang from "./hooks/useLang";
+
+// Components
+import Header from "./components/Header";
 
 function App() {
-  // const { dark, toggle } = useTheme();
-  // const { accent, setAccent } = useAccent();
-  // const { lang, t, toggleLang } = useLang();
+  const { dark, toggle } = useTheme();
+  const { accent, setAccent } = useAccent();
+  const { lang, t, toggleLang } = useLang();
 
   return (
     <div className="bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-white dark:from-neutral-950 via-white dark:via-neutral-950 to-white dark:to-neutral-950 min-h-screen text-neutral-900 dark:text-neutral-100">
-      <h1>Portfolio's Matt</h1>
+      <Header
+        toggleTheme={toggle}
+        darkMode={dark}
+        accent={accent}
+        onPickAccent={setAccent}
+        t={t}
+        lang={lang}
+        onToggleLang={toggleLang}
+      />
     </div>
   );
 }
